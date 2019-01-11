363fa054fde81b14e53b6cde44f01ea44ac0e2cd
Finn <feihu>
2019-01-11 14:42:40 +0800

    小程序js脚本

--- helloworld/pages/wxml/index.js
+++ helloworld/pages/wxml/index.js
@@ -1,4 +1,29 @@
 // pages/wxml/index.js
+/**
+ * JavaScript同浏览器中的 JavaScript 以及 NodeJS 中的 JavaScript 是不相同的
+ * 主要区别参考：https://developers.weixin.qq.com/ebook?action=get_post_info&docid=000a8806958588cb00862bd5851c0a 中2.4.1ECMAScript
+ * 
+ * 小程序中的 JavaScript 是由ECMAScript 以及小程序框架和小程序 API 来实现的。
+ * 
+ * 小程序目前可以运行在三大平台：
+    iOS平台，包括iOS9、iOS10、iOS11
+    Android平台
+    小程序IDE
+    这种区别主要是体现三大平台实现的 ECMAScript 的标准有所不同。
+  截止到当前一共有七个版本的ECMAScript 标准，目前开发者大部分使用的是 ECMAScript 5 和 ECMAScript 6 的标准，但是在小程序中， iOS9和iOS10 所使用的运行环境并没有完全的兼容到 ECMAScript 6 标准，一些 ECMAScript 6 中规定的语法和关键字是没有的或者同标准是有所不同的，例如：箭头函数、let const、模板字符串…
+  因此开发者需要在项目设置中，勾选 ES6 转 ES5 开启此功能，兼容旧版本
+
+  模块化：小程序中可以将任何一个JavaScript 文件作为一个模块，通过module.exports 或者 exports 对外暴露接口。
+
+  脚本的执行顺序：
+    在小程序中的脚本执行顺序有所不同。小程序的执行的入口文件是 app.js 。并且会根据其中 require 的模块顺序决定文件的运行顺序
+    当 app.js 执行结束后，小程序会按照开发者在 app.json 中定义的 pages 的顺序，逐一执行
+
+  作用域：
+    小程序的脚本的作用域同 NodeJS 更为相似。
+    在文件中声明的变量和函数只在该文件中有效，不同的文件中可以声明相同名字的变量和函数，不会互相影响
+    当需要使用全局变量的时，通过使用全局函数 getApp() 获取全局的实例，并设置相关属性值，来达到设置全局变量的目的（注意：当需要保证全局的数据可以在任何文件中安全的被使用到，那么可以在 App() 中进行设置）
+ */
 Page({
 
   /**
